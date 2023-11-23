9793c29ca446e5f8fb75f90ea91934dd52cfbad6
Antonio <marco.a.falcon@outlook.com>
2023-11-23 14:47:19 -0300

    added: register & login

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ const app= express();
 app.use(express.static(path.join(__dirname,'public')))  
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/register',(req,res)=>res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res)=>res.sendFile(path.join(__dirname,'views','login.html')))
 
 app.listen(PORT,console.log(`server running in http://localhost:${PORT}`))
